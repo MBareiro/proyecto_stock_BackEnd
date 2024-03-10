0bc60f94c39e7b32df26f99219dc1f555f34ce10
MBareiro <58647378+MBareiro@users.noreply.github.com>
2024-03-10 18:49:09 -0300

    cambio de url correo

--- controllers/reset_password.py
+++ controllers/reset_password.py
@@ -29,8 +29,8 @@ def forgot_password():
         subject = 'Restablecer Contraseña'
         sender = 'noreply@example.com'
         recipients = [email]
-        """ reset_link = f'https://turnopro-frontend.web.app/reset-password/{reset_token}'  """ # Reemplaza con tu dominio
-        reset_link = f'http://localhost:4200/reset-password/{reset_token}'  # Reemplaza con tu dominio
+        reset_link = f'https://frontendstock.web.app/reset-password/{reset_token}'# Reemplaza con tu dominio
+        """ reset_link = f'http://localhost:4200/reset-password/{reset_token}' """  # Reemplaza con tu dominio
         message = Message(subject=subject, sender=sender, recipients=recipients)
         message.body = f'Para restablecer tu contraseña, sigue este enlace: {reset_link}'        
         try:
