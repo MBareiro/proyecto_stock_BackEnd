e77a0d4c42bce11acf2f3670b6c66bfa8a7b5f5e
MBareiro <58647378+MBareiro@users.noreply.github.com>
2024-03-11 18:36:26 -0300

    cambio en controlador product

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -14,29 +14,45 @@ def get_productos():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/productos/<id>', methods=['GET'])
+@app.route('/producto/<id>', methods=['GET'])
 def get_producto(id):
     producto = Producto.query.get(id)
     return producto_schema.jsonify(producto)
 
 @app.route('/productos', methods=['POST'])
 def create_producto():
-    nombre = request.json['nombre']
-    cantidad = request.json['cantidad']
-    reserva = request.json['reserva']
-    id_categoria = request.json['id_categoria']
+    nombre = request.json.get('nombre')  # Obtener el nombre del producto, si no existe, devolverá None
+    if nombre is None:  # Verificar si se proporcionó el nombre
+        return jsonify({'message': 'El nombre del producto es obligatorio'}), 400
 
-    new_producto = Producto(
-        nombre=nombre,
-        cantidad=cantidad,
-        reserva=reserva,
-        id_categoria=id_categoria
-    )
+    cantidad = request.json.get('cantidad')
+    reserva = request.json.get('reserva')
+    id_categoria = request.json.get('id_categoria')
 
+    # Crear un nuevo producto solo con el nombre proporcionado
+    new_producto = Producto(nombre=nombre)
+
+    # Agregar los otros campos si se proporcionan en la solicitud
+    if cantidad is not None:
+        new_producto.cantidad = cantidad
+    else:
+        new_producto.cantidad = 0
+    if reserva is not None:
+        new_producto.reserva = reserva
+    else:
+        new_producto.reserva = 0
+    if id_categoria is not None:
+        new_producto.id_categoria = id_categoria
+    else:
+        new_producto.id_categoria = 0
+
+
+    # Agregar el nuevo producto a la sesión y confirmar los cambios en la base de datos
     db.session.add(new_producto)
     db.session.commit()
 
     return producto_schema.jsonify(new_producto)
+
 
 @app.route('/productos/<id>', methods=['PUT'])
 def update_producto(id):
